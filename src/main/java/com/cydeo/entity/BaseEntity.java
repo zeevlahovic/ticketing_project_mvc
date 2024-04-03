a5f39aea6757f56c9c36d1b8517316e0042e7178
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-04-02 23:23:39 -0400

    Ticketing App

---
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.cydeo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseEntity {
+
+    private Long id;
+    private LocalDateTime insertDateTime;
+    private Long insertUserID;
+    private LocalDateTime lastUpdateDateTime;
+    private Long lastUpdateUserId;
+}
