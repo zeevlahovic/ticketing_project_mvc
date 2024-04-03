d12f2a6390a953b03728d747952b808caf6b9a1d
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-04-03 16:48:56 -0400

    Ticketing App

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -14,6 +14,7 @@ public class UserController {
     public String createUser(Model model){
 
         model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles");
 
         return "/user/create";
     }
---
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -0,0 +1,15 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+
+import java.util.List;
+
+public interface RoleService {
+
+    RoleDTO save(RoleDTO user);
+    RoleDTO findById(Long id);
+    List<RoleDTO> findAll();
+    void deleteById(Long id);
+
+}
