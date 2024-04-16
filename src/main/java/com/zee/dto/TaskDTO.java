f47484bf609b94ae13de298927ee8a7f1829f221
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-04-16 11:33:41 -0400

    Ticketing App

---
+++ src/main/java/com/zee/dto/TaskDTO.java
@@ -0,0 +1,22 @@
+package com.zee.dto;
+
+import com.zee.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskDTO {
+
+    private Long id;
+    private ProjectDTO project;
+    private UserDTO assignedEmployee;
+    private String taskSubject;
+    private String taskDetail;
+    private Status taskStatus;
+    private LocalDate assignedDate;
+}
---
+++ src/main/java/com/zee/service/TaskService.java
@@ -0,0 +1,6 @@
+package com.zee.service;
+
+import com.zee.dto.TaskDTO;
+
+public interface TaskService extends CrudService<TaskDTO,Long> {
+}
