0bc51c457c4d430b402e4e3ca9ebd23ffb10a961
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-04-17 19:27:07 -0400

    Ticketing App

--- src/main/java/com/zee/boostrap/DataGenerator.java
+++ src/main/java/com/zee/boostrap/DataGenerator.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
-
 @Component
 public class DataGenerator implements CommandLineRunner {
 
@@ -28,38 +27,36 @@ public class DataGenerator implements CommandLineRunner {
         this.roleService = roleService;
         this.userService = userService;
         this.projectService = projectService;
-
         this.taskService = taskService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        RoleDTO adminRole = new RoleDTO(1L, "Admin");
-        RoleDTO managerRole = new RoleDTO(2L, "Manager");
-        RoleDTO employeeRole = new RoleDTO(3L, "Employee");
+        RoleDTO adminRole = new RoleDTO(1L,"Admin");
+        RoleDTO managerRole = new RoleDTO(2L,"Manager");
+        RoleDTO employeeRole = new RoleDTO(3L,"Employee");
 
         roleService.save(adminRole);
         roleService.save(managerRole);
         roleService.save(employeeRole);
 
         UserDTO user1 = new UserDTO("John", "Kesy",
-                "john@gmail.com", "Abc1", true, "7459684532", managerRole, Gender.MALE);
-        UserDTO user5 = new UserDTO("Mike", "Smith",
-                "mike@gmail.com", "Abc2", true, "7459684532", adminRole, Gender.MALE);
-        UserDTO user2 = new UserDTO("Delisa",
-                "Norre", "delisa@gmail.com", "123", true, "8567412358", managerRole, Gender.FEMALE);
-        UserDTO user3 = new UserDTO("Craig", "Jark",
-                "craig@gmail.com", "Abc3", true, "7777775566", employeeRole, Gender.MALE);
-        UserDTO user4 = new UserDTO("Shaun",
-                "Hayns", "shaun@gmail.com", "Abc4", true, "3256987412", managerRole, Gender.MALE);
-        UserDTO user6 = new UserDTO("Elizebeth",
-                "Loren", "elizebeth@gmail.com", "Abc4", true, "5306987412", employeeRole, Gender.FEMALE);
-        UserDTO user7 = new UserDTO("Maria",
-                "Ada", "maria@gmail.com", "Abc4", true, "9996987412", employeeRole, Gender.FEMALE);
-        UserDTO user8 = new UserDTO("Bill",
-                "Matt", "bill@gmail.com", "Abc4", true, "8881239846", employeeRole, Gender.MALE);
-
+                "john@zee.com", "Abc1", "Abc1", true, "7459684532", managerRole, Gender.MALE);
+        UserDTO user2 = new UserDTO("Mike", "Smith",
+                "mike@zee.com", "Abc2", "Abc2", true, "7459684532", adminRole, Gender.MALE);
+        UserDTO user3 = new UserDTO("Delisa", "Norre",
+                "delisa@zee.com",  "Abc3", "Abc3", true, "8567412358", managerRole, Gender.FEMALE);
+        UserDTO user4 = new UserDTO("Craig", "Jark",
+                "craig@zee.com", "Abc4", "Abc4", true, "7777775566", employeeRole, Gender.MALE);
+        UserDTO user5 = new UserDTO("Shaun", "Hayns",
+                "shaun@zee.com", "Abc5", "Abc5", true, "3256987412", managerRole, Gender.MALE);
+        UserDTO user6 = new UserDTO("Elizebeth", "Loren",
+                "elizebeth@zee.com", "Abc6", "Abc6", true, "5306987412", employeeRole, Gender.FEMALE);
+        UserDTO user7 = new UserDTO("Maria", "Ada",
+                "maria@zee.com", "Abc7", "Abc7", true, "9996987412", employeeRole, Gender.FEMALE);
+        UserDTO user8 = new UserDTO("Bill", "Matt",
+                "bill@zee.com", "Abc8", "Abc8", true, "8881239846", employeeRole, Gender.MALE);
 
         userService.save(user1);
         userService.save(user2);
@@ -69,8 +66,6 @@ public class DataGenerator implements CommandLineRunner {
         userService.save(user6);
         userService.save(user7);
         userService.save(user8);
-
-
 
         ProjectDTO project1 = new ProjectDTO("Spring MVC", "PR001", user1, LocalDate.now(), LocalDate.now().plusDays(25), "Creating Controllers", Status.OPEN);
         ProjectDTO project2 = new ProjectDTO("Spring ORM", "PR002", user2, LocalDate.now(), LocalDate.now().plusDays(10), "Creating Database", Status.IN_PROGRESS);
@@ -91,6 +86,5 @@ public class DataGenerator implements CommandLineRunner {
         taskService.save(task4);
 
     }
-
 
 }
--- src/main/java/com/zee/controller/UserController.java
+++ src/main/java/com/zee/controller/UserController.java
@@ -5,7 +5,10 @@ import com.zee.service.RoleService;
 import com.zee.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
@@ -20,21 +23,32 @@ public class UserController {
     }
 
     @GetMapping("/create")
-    public String createUser(Model model) {
+    public String createUser(Model model){
 
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roles", roleService.findAll());
         model.addAttribute("users", userService.findAll());
 
         return "/user/create";
+
     }
 
     @PostMapping("/create")
-    public String insertUser(@ModelAttribute("user") UserDTO user) {
+    public String insertUser(@Valid @ModelAttribute("user") UserDTO user, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("roles", roleService.findAll());
+            model.addAttribute("users", userService.findAll());
+
+            return "/user/create";
+
+        }
 
         userService.save(user);
 
         return "redirect:/user/create";
+
     }
 
     @GetMapping("/update/{username}")
@@ -45,23 +59,31 @@ public class UserController {
         model.addAttribute("users", userService.findAll());
 
         return "/user/update";
+
     }
 
     @PostMapping("/update")
-    public String updateUser(@ModelAttribute("user") UserDTO user) {
+    public String updateUser(@Valid @ModelAttribute("user") UserDTO user, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("roles", roleService.findAll());
+            model.addAttribute("users", userService.findAll());
+
+            return "/user/update";
+
+        }
 
         userService.update(user);
 
         return "redirect:/user/create";
+
     }
 
     @GetMapping("/delete/{username}")
-    public String deleteUser(@PathVariable("username") String username){
-
+    public String deleteUser(@PathVariable("username") String username) {
         userService.deleteById(username);
-
         return "redirect:/user/create";
     }
-
 
 }
